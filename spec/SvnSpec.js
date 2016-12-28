d5be82d6e486b9a773f6abd0d4b9d63871048206
zhouzhongyuan <yawenzhongyuan@gmail.com>
2016-12-28 09:38:20 +0800

    svn 添加测试用例，添加quiet option

--- spec/SvnSpec.js
+++ spec/SvnSpec.js
@@ -2,14 +2,20 @@ import svn from '../src/svn'
 
 describe("svn", () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 2*60*1000;
-    it("svn get", (done) => {
+    it("正确，svn get", (done) => {
         const url = "https://dev.bokesoft.com:9443/svn/mUI/branches/projects/zijiang";
         svn.get(url, 'app/www', 'zhouzy', 'zhouzy')
             .then((result) => {
+                console.log("MESSAGE",result.data);
                 expect(result.success).toBe(true);
                 done();
             })
+    });
+    it("地址错误，svn get", (done) => {
+        const url = "https://dev.bokesoft.com:9443/svn/mUI/branches/projects/zij";
+        svn.get(url, 'app/www', 'zhouzy', 'zhouzy')
             .catch((result) => {
+                console.log("MESSAGE",result.data.message);
                 expect(result.success).toBe(false);
                 done();
             })
--- src/svn.js
+++ src/svn.js
@@ -1,19 +1,16 @@
 import SvnClient from 'svn-spawn';
 
 function svnGet(url,dir,username,password) {
-    console.log(url,dir,username,password);
     return new Promise( (resolve, reject) => {
         const client = new SvnClient({
             cwd: dir,
             username: username,
             password: password
         });
-        client.checkout(url, (err, data) => {
+        client.cmd(['checkout', '--quiet', url], function(err, data) {
             if(err){
-                console.log('ERROR',err);
-                reject( {success: false, data: new Error(err)} )
+                reject( {success: false, data: err} )
             }
-            console.log('SUCCESS',data);
             resolve({success: true, data: data});
         });
     });
